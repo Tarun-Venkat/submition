65b101786a0a52116b28d0026e3e57150e134b4e
Tarun <tarunaei019@gmail.com>
2022-12-01 19:39:14 +0530

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,61 @@
+
+import './App.css';
+
+
+
+
+
+function App() {
+  return (
+    <div className="MainBg">
+      <div>
+      <div className="Title">
+        <img src="https://deepthought.education/assets/images/logo/logo.svg" alt="logo"/>
+        <div className="e"></div>
+      </div>
+      <div>
+        <div className="box1">
+          <div className="name"></div>
+        </div>
+        <div className="nav"></div>
+        <div>
+        <div className="img1" ></div>
+        <div className="img2"></div>
+        <div className="img3"></div>
+        <div className="img4"></div>
+        <div className="img5"></div>
+        <div className="img6"></div>
+        <div className="img7"></div>
+        
+        </div>
+        <button className="bt">Next</button>
+      </div>
+      </div>
+      <div>
+      <div className="Title">
+        <img src="https://deepthought.education/assets/images/logo/logo.svg" alt="logo"/>
+        <div className="e"></div>
+      </div>
+      <div>
+        <div className="box1">
+          <div className="name"></div>
+        </div>
+        <div className="nav"></div>
+        <div>
+        <div className="img1" ></div>
+        <div className="img2"></div>
+        <div className="img3"></div>
+        <div className="img4"></div>
+        <div className="img5"></div>
+        <div className="img6"></div>
+        <div className="img7"></div>
+        
+        </div>
+        <button className="bt">Next</button>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
